0d1781aee7f4777978894af4d5ab9cda90178bbb
grascj <christopher.grasing@stonybrook.edu>
2017-01-30 12:18:35 -0500

    part 1

--- eliza.py
+++ eliza.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 application = Flask(__name__)
@@ -6,20 +6,26 @@ application.debug = True
 
 @application.route("/")
 def hello():
-	return render_template("index.html");
+    return render_template("index.html");
 
 
 @application.route("/eliza/",methods=["GET","POST"])
 def outhere():
-	name=request.form['name']
-	cur=datetime.datetime.now()
-	date=cur.strftime("%Y-%m-%d %H:%M")
-	return date;
-		
+    if(request.method == "POST"):
+        print('POST')
+        name=request.form['name']
+        cur=datetime.datetime.now()
+        date=cur.strftime("%Y-%m-%d %H:%M")
+        return render_template("eliza.html",name=name,date=date);
+    else:
+        print('NOT POST')
+        return render_template("eliza.html");
 
-@application.route("/eliza/DOCTOR/")
+
+@application.route("/eliza/DOCTOR/",methods=["POST"])
 def doktor():
-	return "<h1 style='color:red'>DOKTOR</h1>"
+    
+    return ""
 
 if __name__ == "__main__":
-	application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
