db3b3413ad986df1a34f4232724e8c164796008a
Shane Kennedy <shane.kennedy@stonybrook.edu>
2017-02-23 23:48:28 -0500

    dbio.py done???

--- eliza/dbio.py
+++ eliza/dbio.py
@@ -10,24 +10,52 @@ mysql = MySQL()
 mysql.init_app(app)
 cursor = mysql.connect().cursor() 
 
-def putstatements(humantext, elizatext):
-	cursor.execute('INSERT INTO Statement(timestamp, name, text) \
-		VALUES(NOW(), ' + session['username'] + ', ' + humantext + ');')
-	
-	cursor.execute('INSERT INTO Statement(timestamp, name, text) \
-		VALUES(NOW(),  Eliza, ' + elizatext + ');')
+def putuser(username, password, email):
+	cursor.execute('INSERT INTO ElizaUser(username, password, email) \
+		VALUES(' + username + ', ' + password + ', ' + email + ');')
 
-def getconv(convid):
-	cursor.execute('SELECT * FROM Conversation WHERE convid = ' + convid + \
-		' ORDER BY(timestamp));')
+def putstatements(humantext, elizatext):
+	cursor.execute('INSERT INTO Statement(name, text) \
+		VALUES(' + session['username'] + ', ' + humantext + ');')
+	
+	cursor.execute('INSERT INTO Statement(name, text) \
+		VALUES( Eliza, ' + elizatext + ');')
+
+def putconversation(username)
+	cursor.execute('COUNT * FROM Conversation WHERE username = ' + username')')
+	convid = cursor.getone()
+
+	cursor.execute('INSERT INTO Conversation(username, convid) \
+		VALUES(' + username + ', ' + convid + ');')
+
+def checklogin(username, password):
+	cursor.execute('SELECT * FROM ElizaUser WHERE username = ' + username + ' AND  \
+		password = ' + password + ' AND activated = true')
+
+	user = cursor.getone()
+	if (user == None):
+		return false
+	
+	return true
+
+def getconv(username, convid):
+	cursor.execute('SELECT * FROM Conversation WHERE username = ' + username \
+		+ 'AND convid = ' + convid + ' ORDER BY timestamp DESC')
 	
 	conv = []
 	while ((entry = cursor.getone()) != None):
-		contList.append(entry)
+		conv.append(entry)
 
 	return conv
 
-def getconvlist():
-	cursor.execute('SELECT * FROM'
+def getconvlist(username):
+	cursor.execute('SELECT * FROM Conversation WHERE username = ' + username)
 
-def put
+	convlist = []
+	while ((conv = cursor.getone()) != None):
+		convlist.append(conv)
+
+	return convlist
+
+
+
--- eliza/elizaTables.sql
+++ eliza/elizaTables.sql
@@ -4,14 +4,14 @@ CREATE TABLE ElizaUser (
 	username CHAR(30) NOT NULL,
 	password CHAR(30) NOT NULL,
 	email CHAR(50) NOT NULL,
-	status BOOLEAN DEFAULT false,
+	activated BOOLEAN DEFAULT false,
 	PRIMARY KEY (username)
 );
 
 CREATE TABLE Conversation (
 	username CHAR(30) NOT NULL,
 	convid INTEGER NOT NULL,
-	startdate DATE NOT NULL,
+	startdate DATE NOT NULL DEFAULT CURDATE(),
 	PRIMARY KEY (convid, username), 
 	FOREIGN KEY (username) REFERENCES ElizaUser(username)
 );
