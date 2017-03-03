36130d07c6b001181874c3450f3841d14273c63e
Shane Kennedy <shane.kennedy@stonybrook.edu>
2017-03-02 19:29:19 -0500

    mongogit add .git add . @#$%

--- eliza/dbio.py
+++ eliza/dbio.py
@@ -5,46 +5,51 @@ from flask_pymongo import PyMongo
 # database setup
 eliza.application.config['MONGO_DBNAME'] = 'elizaDB'
 mongo = PyMongo(eliza.application)
-userTable = mongo.db.user
-statementTable = mongo.db.statement
-conversationTable = mongo.db.conversation
+#userTable = mongo.db.user
+#statementTable = mongo.db.statement
+#conversationTable = mongo.db.conversation
 
 
 def putuser(username, password, email):
-    userTable.insert_one({'username': username, 'password': password,
-                          'email': email, 'activated': 'false', 'cookie': ''})
+    mongo.db.userTable.insert_one({'username': username, 'password': password,
+                                   'email': email, 'activated': 'false',
+                                   'cookie': ''})
     return None
 
 
 def activateuser(username, key):
-    user = userTable.find_one({'username': username, 'key': key})
+    user = mongo.db.userTable.find_one({'username': username, 'key': key})
     if (user is not None or key == 'abracadabra'):
-        userTable.update_one({'username': username},
-                             {'$set': {'activated': 'true'}})
+        mongo.db.userTable.update_one({'username': username},
+                                      {'$set': {'activated': 'true'}})
         return True
     else:
         return False
 
 
 def putstatements(username, convid, humantext, elizatext):
-    statementTable.insert_one({'username': username, 'convid': convid,
-                               'timestamp': datetime.now(), 'name': username,
-                               'text': humantext})
+    mongo.db.tatementTable.insert_one({'username': username, 'convid': convid,
+                                       'timestamp': datetime.now(),
+                                       'name': username,
+                                       'text': humantext})
 
-    statementTable.insert_one({'username': username, 'convid': convid,
-                               'timestamp': datetime.now(), 'name': username,
-                               'text': humantext})
+    mongo.db.statementTable.insert_one({'username': username, 'convid': convid,
+                                        'timestamp': datetime.now(),
+                                        'name': username,
+                                        'text': humantext})
 
 
 def putconversation(username, convid):
-    conversationTable.insert_one({'username': username, 'convid': convid,
-                                  'startdate': datetime.today()})
+    mongo.db.conversationTable.insert_one({'username': username,
+                                           'convid': convid,
+                                           'startdate': datetime.today()})
     return None
 
 
 def checklogin(username, password):
-    user = userTable.find_one({'username': username, 'password': password,
-                               'activated': 'true'})
+    user = mongo.db.userTable.find_one({'username': username,
+                                        'password': password,
+                                        'activated': 'true'})
     if (user is not None):
         return True
     else:
@@ -52,18 +57,19 @@ def checklogin(username, password):
 
 
 def putcookie(username, cookie):
-    userTable.update_one({'username': username},
-                         {'$set': {'cookie': cookie}})
+    mongo.db.userTable.update_one({'username': username},
+                                  {'$set': {'cookie': cookie}})
     return None
 
 
 def getuser(cookie):
-    user = userTable.find_one({'cookie': cookie})
+    user = mongo.db.userTable.find_one({'cookie': cookie})
     return user.username
 
 
 def getconv(username, convid):
-    statelist = statementTable.find({'username': username, 'convid': convid})
+    statelist = mongo.db.statementTable.find({'username': username,
+                                              'convid': convid})
 
     conv = []
     for entry in statelist:  # sorted(statelist.iterkeys()):
@@ -73,7 +79,7 @@ def getconv(username, convid):
 
 
 def getconvlist(username):
-    convlist = conversationTable.find({'username': username})
+    convlist = mongo.db.conversationTable.find({'username': username})
     return convlist
 
 
